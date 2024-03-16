8835f80c03b999ef6822d65761aaab5bfd95b693
LPCBCN044 <alexmrtc@gmail.com>
2024-03-16 19:14:02 +0100

    Finish number exercises

---
+++ js/numbers.js
@@ -0,0 +1,50 @@
+function addition(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function substraction(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiplication(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function division(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function exponientation(firstNumber, secondNumber) {
+    return Math.pow(firstNumber, secondNumber);
+}
+
+function getRemainder(firstNumber, secondNumber) {
+    return firstNumber % secondNumber;
+}
+
+function squareRoot(number) {
+    return Math.sqrt(number);
+}
+
+function absoluteValue(number) {
+    return Math.abs(number)
+}
+
+function roundNumber(decimalNumber) {
+    return Math.round(decimalNumber)
+}
+
+function generateRandomNumber() {
+    return Math.round(Math.random());
+}
+
+// console.log(addition(49, 78));
+// console.log(substraction(4, 78));
+// console.log(multiplication(4, 8));
+// console.log(division(49, 7));
+// console.log(exponientation(4, 3));
+// console.log(getRemainder(48, 3));
+// console.log(squareRoot(64));
+// console.log(absoluteValue(-3));
+// console.log(roundNumber(49.78));
+// console.log(generateRandomNumber());
